ce53443d3a5607f381c3dc3f60103eccc647cbb7
Branden Keck <bmkeck62@gmail.com>
2021-10-21 07:36:03 -0400

    Updated with basic swap code

---
+++ arbritrage/algo/simple_swap.py
@@ -0,0 +1,34 @@
+# Imports
+import json, time
+from datetime import datetime
+from decimal import Decimal
+from algosdk import mnemonic
+
+# Load JSON keys (untracked in repo for security)
+f = open('keys.json',)
+keys = json.load(f)
+f.close()
+
+# Get Algo Client / Using purestake; supplement your own API key for the algod_token
+from algosdk.v2client import algod
+algod_address = "https://mainnet-algorand.api.purestake.io/ps2"
+algod_token = keys['algod_token_2']
+headers = {"X-API-Key": algod_token}
+algod_client = algod.AlgodClient(algod_token, algod_address, headers)
+
+# Get TMan Client / 350338509 is the app ID for all TinymanClient implementations
+from tinyman.v1.client import TinymanClient
+client = TinymanClient(algod_client, 350338509)
+
+# Create a Trade
+CHOICE = client.fetch_asset(297995609)
+ALGO = client.fetch_asset(0)
+CHOICE_ALGO = client.fetch_pool(CHOICE, ALGO)
+quote_CHOICE_ALGO = CHOICE_ALGO.fetch_fixed_input_swap_quote(CHOICE(100), slippage=0.001)
+tx_CHOICE_ALGO = CHOICE_ALGO.prepare_swap_transactions_from_quote(quote_CHOICE_ALGO)
+
+# Sign the transaction
+private_key = mnemonic.to_private_key(keys['mnemonic'])
+tx_CHOICE_ALGO.sign_with_private_key(keys['address'], private_key)
+result = client.submit(tx_CHOICE_ALGO, wait=True)
+print(result)
--- arbritrage/algo/track_choice.py
+++ arbritrage/algo/track_choice.py
@@ -1,6 +1,7 @@
 # Imports and JSON keys (API key untracked in repo for security)
 import json, time
 from datetime import datetime
+from decimal import Decimal
 f = open('keys.json',)
 keys = json.load(f)
 f.close()
@@ -8,7 +9,7 @@ f.close()
 # Get Algo Client / Using purestake; supplement your own API key for the algod_token
 from algosdk.v2client import algod
 algod_address = "https://mainnet-algorand.api.purestake.io/ps2"
-algod_token = keys['algod_token']
+algod_token = keys['algod_token_2']
 headers = {"X-API-Key": algod_token}
 algod_client = algod.AlgodClient(algod_token, algod_address, headers)
 
@@ -23,18 +24,28 @@ USDC = client.fetch_asset(31566704)
 
 # Fetch the pool we will work with
 ALGO_USDC = client.fetch_pool(ALGO, USDC)
-CHOICE_ALGO = client.fetch_pool(CHOICE, ALGO)
+CHOICE_ALGO = client.fetch_pool(CHOICE, ALGO) # Two Part Swap CHOICE -> ALGO -> USDC
+CHOICE_USDC = client.fetch_pool(CHOICE, USDC) # Direct Comparison CHOICE -> USDC
 
 # Take a loog at some swaps
 while True:
 
-    # Calculate Price
-    # Double swap CHOICE -> ALGO -> USDC due to low ALGO-USDC liquidity
+    # Calculate ALGO -> USDC Swap
     quote_ALGO_USDC = ALGO_USDC.fetch_fixed_input_swap_quote(ALGO(1_000_000), slippage=0)
+    algo_out = quote_ALGO_USDC.amount_out_with_slippage
+    algo_price = Decimal(algo_out.amount) / Decimal(10**algo_out.asset.decimals)
+
+    # Calculate CHOICE -> ALGO Swap
     quote_CHOICE_ALGO = CHOICE_ALGO.fetch_fixed_input_swap_quote(CHOICE(100), slippage=0)
-    decimal_correction = 10 ** (CHOICE.decimals - ALGO.decimals)
-    algo_price = quote_ALGO_USDC.price
-    choice_price = quote_ALGO_USDC.price * (decimal_correction * quote_CHOICE_ALGO.price)
+    choice_out = quote_CHOICE_ALGO.amount_out_with_slippage
+    choice_out_qty = Decimal(choice_out.amount) / Decimal(10**choice_out.asset.decimals)
+    choice_price = algo_price * choice_out_qty
+
+    # Calculate CHOICE -> USDC Swap to compare
+    quote_CHOICE_USDC = CHOICE_USDC.fetch_fixed_input_swap_quote(CHOICE(100), slippage=0)
+    direct_choice_out = quote_CHOICE_USDC.amount_out_with_slippage
+    direct_choice_price = Decimal(direct_choice_out.amount) / Decimal(10**direct_choice_out.asset.decimals)
+
 
     # Get timestamp
     now = datetime.now()
@@ -43,8 +54,9 @@ while True:
     # Print results
     print(f'\r')
     print(f'[{current_time}]')
-    print(f'Price of ALGO in USDC: {algo_price}')
-    print(f'Price of CHOICE in USDC: {choice_price}')
+    print(f'ALGO -> USDC (Direct Swap): {algo_price}')
+    print(f'CHOICE -> ALGO -> USDC Rate: {choice_price}')
+    print(f'CHOICE -> USDC (Direct Swap): {direct_choice_price}')
 
     # Wait ten seconds before checking again
     time.sleep(10)
