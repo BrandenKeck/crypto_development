d431b13af526924a503b62dede970b4758e043c2
EC2 Default User <ec2-user@ip-172-31-28-207.us-east-2.compute.internal>
2021-11-12 15:07:10 +0000

    bots in the cloud

---
+++ launch_discord_bots.sh
@@ -0,0 +1,3 @@
+cd ~/crypto_development/discord_bots/bots
+screen -dmS bots python3 -u choice_price.py > ~/crypto_development/log/botprice.output
+screen -dmS bots python3 -u choice_price_commands.py > ~/crypto_development/log/botcmds.output
