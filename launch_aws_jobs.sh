2bea62754efb8fee9311bcbdf198fcd5c6d6211a
Branden Keck <bmkeck62@gmail.com>
2021-11-15 09:08:51 -0500

    Azazello Ready

--- launch_aws_jobs.sh
+++ launch_aws_jobs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/crypto_development/algo_buy_sell_models/models
-python3 -u tengu.py > ~/crypto_development/log/tengu.output &
+python3 -u azazello.py > ~/crypto_development/log/azazello.output &
