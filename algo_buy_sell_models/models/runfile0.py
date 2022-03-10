a00918ffd8a1e7269ac5b2fb7bd33d340d9595d4
Branden Keck <bkeck@its.jnj.com>
2022-03-10 10:47:32 -0500

    updated azazello config for new trial

--- algo_buy_sell_models/models/runfile0.py
+++ algo_buy_sell_models/models/runfile0.py
@@ -1,3 +1,8 @@
 from azazello import azazello
-aa = azazello()
+aa = azazello(
+    default_model = False,
+    load_model = False
+)
+aa.add_tengu(0, 31566704, alpha=0.040, beta=0.060, gamma=1.000) # ALGO / USDC
+aa.add_tengu(0, 226701642, alpha=0.020, beta=0.020, gamma=0.500) # ALGO / YLDY
 aa.run()
--- launch_aws_jobs.sh
+++ launch_aws_jobs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/crypto_development/algo_buy_sell_models/models
-python3 -u azazello.py > ~/crypto_development/log/azazello.output &
+python3 -u runfile0.py > ~/crypto_development/log/azazello.output &
