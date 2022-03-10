b36eeef1f4119934c2e15e3d7a0d82026e02a0ce
Branden Keck <bkeck@its.jnj.com>
2022-03-10 10:52:39 -0500

    corrected typo

--- launch_aws_jobs.sh
+++ launch_aws_jobs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/crypto_development/algo_buy_sell_models/models
-python3 -u runfile0.py > ~/crypto_development/log/azazello.output &
+cd ~/crypto_dev/algo_buy_sell_models/models
+python3 -u runfile0.py > ~/crypto_dev/log/azazello.output &
